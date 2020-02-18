bc44c7b781143cac556e2ef9872467e98d45c9d8
Su <grat3su@gmail.com>
2020-02-18 17:31:15 +0900

    newproj

---
+++ Across/Assets/Editor/MapEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MapEditor : EditorWindow
+{
+    [MenuItem("Custom/MapEditor")]
+    static void Init()
+    {
+        MapEditor window = (MapEditor)EditorWindow.GetWindow(typeof(MapEditor));
+    }
+    int mapsize;
+    void OnGUI()
+    {
+        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+        mapsize = EditorGUILayout.IntField("Map Size", mapsize);
+
+        
+    }
+    
+}
